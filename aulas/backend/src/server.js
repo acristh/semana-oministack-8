15d87a3f10bb1483b39476c49f8ee088df66dc45
André Cristiano <andre.cristiano1975@gmail.com>
2019-08-08 00:26:06 -0300

    feat: Add routes, config schema and created controler

--- aulas/backend/src/server.js
+++ aulas/backend/src/server.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const routes = require('./routes');
 
 const server = express();
 
+mongoose.connect('mongodb://oministack:oministack@localhost/dev-tinder?retryWrites=true&w=majority', {useNewUrlParser: true});
+
+server.use(express.json());
 server.use(routes);
 
 server.listen(3333);
