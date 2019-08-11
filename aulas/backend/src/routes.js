5f7f369c019c3b3362fefb06236ce33acf1b3caf
André Cristiano <andre.cristiano1975@gmail.com>
2019-08-11 00:53:25 -0300

    feat: created DislikeControler and LikeController

--- aulas/backend/src/routes.js
+++ aulas/backend/src/routes.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const DevController = require('./controllers/DevController')
-
+const LikeController = require('./controllers/LikeController');
+const DislikeController = require('./controllers/DislikeContoller');
 const routes = express.Router();
 
+routes.get('/devs', DevController.index);
 routes.post('/devs', DevController.store);
+routes.post('/devs/:devid/likes', LikeController.store);
+routes.post('/devs/:devid/dislikes', DislikeController.store);
 
 module.exports = routes;
